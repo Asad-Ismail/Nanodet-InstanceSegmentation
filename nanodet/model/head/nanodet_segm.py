4a68a3769120673fdd2706b082dc98203c1c2408
Asad-Ismail <asadismaeel@gmail.com>
2023-05-24 19:47:39 +0000

    updated nano segementation

--- nanodet/model/head/nanodet_segm.py
+++ nanodet/model/head/nanodet_segm.py
@@ -1,22 +1,7 @@
-# Copyright 2021 RangiLyu.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import torch
 import torch.nn as nn
 from torchvision.ops import roi_align
 import numpy as np
-
 
 from ..module.conv import ConvModule, DepthwiseConvModule
 from ..module.init_weights import normal_init
